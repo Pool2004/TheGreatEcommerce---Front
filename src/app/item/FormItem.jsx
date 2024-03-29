a4f5c0e929fec458f930fae24631c2af4b109119
Nicolas Londono <nicksiuxs@gmail.com>
2024-03-28 19:48:13 -0500

    Add: create item

---
+++ src/app/item/FormItem.jsx
@@ -0,0 +1,78 @@
+"use client";
+import Input from "@/app/components/Input";
+import useForm from "@/app/customHooks/useForm";
+
+/**
+ * @description component that show the form item
+ * @returns Form Item Componet
+ */
+const FormItem = () => {
+  const { name, description, price, quantity, img, onInputChange } = useForm({
+    name: "",
+    description: "",
+    price: 0,
+    quantity: 0,
+    img: "",
+  });
+
+  /**
+   * @description function to handle the submit event
+   * @param {*} event
+   */
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    const item = {
+      name,
+      description,
+      price,
+      quantity,
+      img,
+    };
+    console.log("articulo nuevo: >>>>", item);
+  };
+
+  return (
+    <form onSubmit={handleOnSubmit} className="w-full mb-7">
+      <Input
+        label={"Nombre:"}
+        name="name"
+        value={name}
+        onChange={onInputChange}
+        styles={"mb-3"}
+      />
+      <Input
+        label={"Descripción:"}
+        name="description"
+        value={description}
+        onChange={onInputChange}
+        styles={"mb-3"}
+      />
+      <Input
+        label={"Precio:"}
+        name="price"
+        value={price}
+        onChange={onInputChange}
+        styles={"mb-3"}
+      />
+      <Input
+        label={"Cantidad:"}
+        name="quantity"
+        value={quantity}
+        onChange={onInputChange}
+        styles={"mb-3"}
+      />
+      <Input
+        label={"URL de la imagen:"}
+        name="img"
+        value={img}
+        onChange={onInputChange}
+        styles={"mb-3"}
+      />
+      <button className="bg-indigo-600 text-white border-none font-medium rounded py-2 px-3 w-full">
+        Crear producto
+      </button>
+    </form>
+  );
+};
+
+export default FormItem;
--- src/app/item/create/page.jsx
+++ src/app/item/create/page.jsx
@@ -1,45 +1,10 @@
-"use client";
-import Input from "@/app/components/Input";
-import useForm from "@/app/customHooks/useForm";
+import FormItem from "../FormItem";
 
 export default function Home() {
-  const { name, description, img, onInputChange } = useForm({
-    name: "",
-    description: "",
-    img: "",
-  });
-
-  const handleOnSubmit = (event) => {
-    event.preventDefault();
-    console.log("articulo nuevo: >>>>", { name, description, img });
-  };
   return (
     <main>
       <h1 className="text-3xl font-bold text-center ">Crear Artículo</h1>
-      <form onSubmit={handleOnSubmit} className="w-full mb-7">
-        <Input
-          label={"Nombre"}
-          name="name"
-          value={name}
-          onChange={onInputChange}
-          styles={"mb-3"}
-        />
-        <Input
-          label={"Descripcion"}
-          name="description"
-          value={description}
-          onChange={onInputChange}
-          styles={"mb-3"}
-        />
-         <Input
-          label={"Imagen URL"}
-          name="img"
-          value={img}
-          onChange={onInputChange}
-          styles={"mb-3"}
-        />
-        <button className="bg-indigo-600 text-white border-none font-medium rounded py-2 px-3 w-full">Crear producto</button>
-      </form>
+      <FormItem />
     </main>
   );
 }
