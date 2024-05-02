234c2c91704fa66640f4e4e46c6679dfdcad5ce3
Nicolas Londono <nicksiuxs@gmail.com>
2024-05-01 23:49:59 -0500

    Add: checkpoint table

---
+++ src/app/backoffice/order/OrdersPage.jsx
@@ -0,0 +1,12 @@
+"use clients";
+import React from "react";
+
+const OrdersPage = () => {
+  return (
+    <main className="mx-auto max-w-7xl p-6 lg:px-8 lg:py-20">
+      <h1>Ordenes</h1>
+    </main>
+  );
+};
+
+export default OrdersPage;
