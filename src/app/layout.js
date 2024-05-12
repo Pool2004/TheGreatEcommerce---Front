aa2af67f9f64599e6727006e02cf199ff5661d63
Nicolas Londono <nicksiuxs@gmail.com>
2024-05-12 09:42:24 -0500

    Add: react hot toast

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import Provider from "../redux/Provider";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { Toaster } from "react-hot-toast";
 
 export const metadata = {
   title: "The Great Ecommerce",
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
           <Header />
           {children}
           <Footer />
+          <Toaster position="top-center" />
         </body>
       </Provider>
     </html>
